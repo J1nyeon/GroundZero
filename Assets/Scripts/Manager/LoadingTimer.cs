7022d0e8b86381590ffc3542fa2b92bbc2c1cda6
JungDongYeol <godf202@gmail.com>
2026-09-26 02:02:15 +0900

    인게임 UI진행중

--- Assets/Scripts/Manager/LoadingTimer.cs
+++ Assets/Scripts/Manager/LoadingTimer.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingTimer : MonoBehaviour
 {
     public float timer;
     public TextMeshProUGUI timerTxt;
+
+    public string gameScene = "GameScene";
 
     void Update()
     {
@@ -14,10 +17,19 @@ public class LoadingTimer : MonoBehaviour
         if (timer <= 0)
         {
             timer = 0;
+            StartCoroutine(CoSceneChangeDelay());
         }
         int sec = (int)timer;
         int milsec = (int)((timer % 1) * 1000);
         timerTxt.text = $"00:{sec:00}.{milsec:000}";
         
+    }
+
+    public IEnumerator CoSceneChangeDelay()
+    {
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene(gameScene);
+
     }
 }
